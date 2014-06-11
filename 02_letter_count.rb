e2b9c4ddae2eaeebd04b476642abe7be64bc6e1c
Wuang Qin <qinwuang@live.unc.edu>
2014-06-11 16:19:13 -0400

    inital solution

--- 02_letter_count.rb
+++ 02_letter_count.rb
@@ -1,2 +1,15 @@
+# Write a function, `letter_count(str)` that takes a string and
+# returns a hash mapping each letter to its frequency. Do not include
+# spaces.
+#
 def letter_count(str)
+     counts = {}
+     str.each_char do |char|
+        next if char==" "
+        counts[char] =0 unless counts.include?(char)
+        counts[char]+=1	
+        end
+     counts  
 end
+
+puts letter_count("jack")
