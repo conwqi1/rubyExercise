6379f01b48e63ca2485a58eded5a58b9789c4590
Wuang Qin <qinwuang@live.unc.edu>
2014-06-11 20:58:30 -0400

    initial solution

--- 05_morse_code.rb
+++ 05_morse_code.rb
@@ -1,2 +1,61 @@
-def morse_encode(str)
+
+# Build a function, `morse_encode(str)` that takes in a string (no
+# numbers or punctuation) and outputs the morse code for it. See
+# http://en.wikipedia.org/wiki/Morse_code. Put two spaces between
+# words and one space between letters.
+#
+# You'll have to type in morse code: I'd use a hash to map letters to
+# codes. Don't worry about numbers.
+#
+# I wrote a helper method `morse_encode_word(word)` that handled a
+# single word.
+#
+MORSE_CODE = {
+  "a" => ".-",
+  "b" => "-...",
+  "c" => "-.-.",
+  "d" => "-..",
+  "e" => ".",
+  "f" => "..-.",
+  "g" => "--.",
+  "h" => "....",
+  "i" => "..",
+  "j" => ".---",
+  "k" => "-.-",
+  "l" => ".-..",
+  "m" => "--",
+  "n" => "-.",
+  "o" => "---",
+  "p" => ".--.",
+  "q" => "--.-",
+  "r" => ".-.",
+  "s" => "...",
+  "t" => "-",
+  "u" => "..-",
+  "v" => "...-",
+  "w" => ".--",
+  "x" => "-..-",
+  "y" => "-.--",
+  "z" => "--.."
+}
+def morse_encode_word(word)
+   newWord=[]
+   word.each_char do |char|
+       if MORSE_CODE.include?(char)
+          newWord<<MORSE_CODE[char]
+       else
+          newWord<<char
+       end
+   end
+   newWord.join(' ')
 end
+puts morse_encode_word("a")
+puts morse_encode_word("cat")
+# "-.-. .- -"
+
+def morse_encode(str)
+    newStr=[]
+    newStr=str.split.map{|word|morse_encode_word(word)}
+    newStr.join("  ")
+end
+
